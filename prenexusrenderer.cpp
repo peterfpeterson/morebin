00c22500bd2cb5923c36122162560e7caa2c1b67
peterfpeterson <peterfpeterson@gmail.com>
2011-09-21 16:28:11 -0400

    Bumping up the block size.

--- prenexusrenderer.cpp
+++ prenexusrenderer.cpp
@@ -20,7 +20,7 @@ namespace { // anonymous namespace
 
 static const string EOL("\n");
 
-static const size_t BLOCK_SIZE = 1024;
+static const size_t BLOCK_SIZE = 4096;
 
 AllowedTypes getTypes() {
   AllowedTypes types;
