c908d6ca0ce249569fc42bd138b7fe40297a471c
Pete Peterson <petersonpf@ornl.gov>
2015-05-13 15:19:16 -0400

    working with boost more

--- appveyor.cmake
+++ appveyor.cmake
@@ -1,9 +1,1 @@
 set(BOOST_ROOT C:\projects\morebin\boost.1.58.0.0\lib\native CACHE PATH "")
-set(BOOST_DATE_TIME_LIBRARY_RELEASE C:\projects\morebin\boost_date_time-vc120.1.58.0.0\lib\native\address-model-64\lib\boost_date_time-vc120-mt-1_58.dll
-   CACHE PATH "")
-set(BOOST_DATE_TIME_LIBRARY_DEBUG C:\projects\morebin\boost_date_time-vc120.1.58.0.0\lib\native\address-model-64\lib\boost_date_time-vc120-mt-gd-1_58.dll
-   CACHE PATH "")
-set(BOOST_PROGRAM_OPTIONS_LIBRARY_RELEASE C:\projects\morebin\boost_program_options-vc120.1.58.0.0\lib\native\address-model-64\lib\boost_program_options-vc120-mt-1_58.dll
-   CACHE PATH "")
-set(BOOST_PROGRAM_OPTIONS_LIBRARY_RELEASE C:\projects\morebin\boost_program_options-vc120.1.58.0.0\lib\native\address-model-64\lib\boost_program_options-vc120-mt-gd-1_58.dll
-   CACHE PATH "")
