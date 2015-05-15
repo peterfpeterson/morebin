4e3b9d812c107ca9d7c0587acc476ad87e8e3067
unknown <svh@pc83407.ornl.gov>
2015-05-15 11:31:47 -0400

    new appveyor cache file

--- appveyor.cmake
+++ appveyor.cmake
@@ -1,1 +1,6 @@
-set(BOOST_ROOT "C:\projects\morebin\boost.1.58.0.0\lib\native" CACHE PATH "")
+set(BUILD_DIR C:/projects/morebin CACHE PATH "")
+set(Boost_INCLUDE_DIR ${BUILD_DIR}/boost.1.58.0.0/lib/native/include CACHE PATH "")
+set(Boost_DATE_TIME_LIBRARY_RELEASE ${BUILD_DIR}/boost_date_time-vc120.1.58.0.0/lib/native/address-model-64/lib/boost_date_time-vc120-mt-1_58.dll CACHE PATH "")
+set(Boost_DATE_TIME_LIBRARY_DEBUG ${BUILD_DIR}/boost_date_time-vc120.1.58.0.0/lib/native/address-model-64/lib/boost_date_time-vc120-mt-gd-1_58.dll CACHE PATH "")
+set(Boost_PROGRAM_OPTIONS_LIBRARY_RELEASE ${BUILD_DIR}/boost_program_options-vc120.1.58.0.0/lib/native/address-model-64/lib/boost_program_options-vc120-mt-1_58.dll CACHE PATH "")
+set(Boost_PROGRAM_OPTIONS_LIBRARY_DEBUG ${BUILD_DIR}/boost_program_options-vc120.1.58.0.0/lib/native/address-model-64/lib/boost_program_options-vc120-mt-gd-1_58.dll CACHE PATH "")
