0fe8180af353b55010abd1c7444ddb9e87b8933e
Pete Peterson <petersonpf@ornl.gov>
2016-05-10 13:56:32 -0400

    Attempt to fix appveyor builds
    ...by using their install of the boost libraries

--- appveyor.cmake
+++ appveyor.cmake
@@ -1,6 +1,1 @@
 set(BUILD_DIR C:/projects/morebin CACHE PATH "")
-set(Boost_INCLUDE_DIR ${BUILD_DIR}/boost.1.58.0.0/lib/native/include CACHE PATH "")
-set(Boost_DATE_TIME_LIBRARY_RELEASE ${BUILD_DIR}/boost_date_time-vc120.1.58.0.0/lib/native/address-model-64/lib/boost_date_time-vc120-mt-1_58.lib CACHE PATH "")
-set(Boost_DATE_TIME_LIBRARY_DEBUG ${BUILD_DIR}/boost_date_time-vc120.1.58.0.0/lib/native/address-model-64/lib/boost_date_time-vc120-mt-gd-1_58.lib CACHE PATH "")
-set(Boost_PROGRAM_OPTIONS_LIBRARY_RELEASE ${BUILD_DIR}/boost_program_options-vc120.1.58.0.0/lib/native/address-model-64/lib/boost_program_options-vc120-mt-1_58.lib CACHE PATH "")
-set(Boost_PROGRAM_OPTIONS_LIBRARY_DEBUG ${BUILD_DIR}/boost_program_options-vc120.1.58.0.0/lib/native/address-model-64/lib/boost_program_options-vc120-mt-gd-1_58.lib CACHE PATH "")
