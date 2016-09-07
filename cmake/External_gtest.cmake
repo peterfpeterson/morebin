2f023c7dc0c7d38745ec0af78fe648ccc410370b
Pete Peterson <petersonpf@ornl.gov>
2016-09-07 11:47:27 -0400

    Fix the gtest external project

--- cmake/External_gtest.cmake
+++ cmake/External_gtest.cmake
@@ -4,18 +4,52 @@ find_package(Threads REQUIRED)
 
 include(ExternalProject)
 
-# Download and install GoogleTest
+set(GTEST_PREFIX "googletest")
+set(GTEST_DIR "${CMAKE_CURRENT_BINARY_DIR}/${GTEST_PREFIX}")
+ExternalProject_Add(
+    googletest-${gtest_version}
+    DOWNLOAD_DIR "${GTEST_DIR}/src" # The only dir option which is required
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG "release-${gtest_version}"
+    PREFIX "${GTEST_PREFIX}"
+    TIMEOUT 10
+    LOG_DOWNLOAD ON
+
+    # Disable all other steps
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+)
+
+# Build gtest from existing sources
 ExternalProject_Add(
     gtest
-    URL "http://googletest.googlecode.com/files/gtest-${gtest_version}.zip"
-    URL_MD5 "${gtest_md5}"
-    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
-    # Disable install step
+    DOWNLOAD_COMMAND "" # No download required
+    SOURCE_DIR "${GTEST_DIR}/src/googletest-${gtest_version}" # Use specific source dir
+    PREFIX "${GTEST_PREFIX}" # But use prefix for compute other dirs
     INSTALL_COMMAND ""
-    CMAKE_ARGS
-      -DBUILD_SHARED_LIBS:BOOL=ON
-#      -Dgtest_force_shared_crt:BOOL=ON
+    LOG_CONFIGURE ON
+    LOG_BUILD ON
+    CMAKE_ARGS -DBUILD_SHARED_LIBS:BOOL=ON
 )
+
+# gtest should be build after being downloaded
+add_dependencies(gtest googletest-${gtest_version})
+
+# Build gmock from existing sources
+ExternalProject_Add(
+    gmock
+    DOWNLOAD_COMMAND "" # No download required
+    SOURCE_DIR "${GTEST_DIR}/src/googletest-${gtest_version}" # Use specific source dir
+    #SOURCE_DIR "${GTEST_DIR}/src/googlemock" # Use specific source dir
+    PREFIX "${GTEST_PREFIX}" # But use prefix for compute other dirs
+    INSTALL_COMMAND ""
+    CMAKE_ARGS -DBUILD_SHARED_LIBS:BOOL=ON
+    LOG_CONFIGURE ON
+    LOG_BUILD ON
+)
+
+add_dependencies(gmock googletest-${gtest_version})
 
 # Create a libgtest target to be used as a dependency by test programs
 add_library(libgtest IMPORTED SHARED GLOBAL)
