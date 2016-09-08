0d8c9c40e5a0416ae89001ef659a8706836b3e77
Pete Peterson <petersonpf@ornl.gov>
2016-09-08 17:09:25 -0400

    Diagnosing by build script

--- appveyor.cmake
+++ appveyor.cmake
@@ -1,2 +1,3 @@
 set(BUILD_DIR C:/projects/morebin CACHE PATH "")
 set(BOOST_ROOT C:/Libraries/boost_1_60_0/lib64-msvc-14.0 CACHE_PATH "")
+set(BOOST_DIR C:/Libraries/boost_1_60_0/lib64-msvc-14.0 CACHE_PATH "")
