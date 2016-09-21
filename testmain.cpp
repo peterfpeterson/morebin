479fc65c56202f4193ef9d0edb15ee2449b5e062
Pete Peterson <petersonpf@ornl.gov>
2016-09-21 14:42:06 -0400

    Adding unhandled exceptions handler
    Taken from
    https://stackoverflow.com/questions/3355683/c-stack-trace-from-unhandled-exception/3356421#3356421

--- testmain.cpp
+++ testmain.cpp
@@ -1,8 +1,24 @@
 #include "gtest/gtest.h"
+#include <exception>
+#include <execinfo.h>
+
+void handler() {
+  void *trace_elems[20];
+  int trace_elem_count(backtrace(trace_elems, 20));
+  char **stack_syms(backtrace_symbols(trace_elems, trace_elem_count));
+  for (int i = 0; i < trace_elem_count; ++i) {
+    std::cout << stack_syms[i] << "\n";
+  }
+  free(stack_syms);
+
+  exit(1);
+}
 
 int main(int argc, char **argv)
 {
-    ::testing::InitGoogleTest(&argc, argv);
-    int ret = RUN_ALL_TESTS();
-    return ret;
+  std::set_terminate(handler);
+
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret = RUN_ALL_TESTS();
+  return ret;
 }
